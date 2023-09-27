b9a9af5a1ccfa616d19c322803ad1072e7db6330
Darshil Thummar <thummardarshil9687@gmail.com>
2023-09-27 18:54:16 +0530

    Create Sort 0 1 2.java

---
+++ Sort 0 1 2.java
@@ -0,0 +1,34 @@
+import java.util.* ;
+import java.io.*; 
+public class Solution 
+{
+    public static void sort012(int[] nums)
+    {
+        int n = nums.length;
+        int zero = 0;
+        int one = 0;
+        int two = 0;
+
+        for(int i=0; i<n; i++){
+            if(nums[i] == 0){
+                zero++;
+            }
+            else if(nums[i] == 1){
+                one++;
+            }
+            else{
+                two++;
+            }
+        }
+
+        for(int i = 0; i < zero; i++){
+            nums[i] = 0;
+        }
+        for(int i = zero; i < (zero + one); i++){
+            nums[i] = 1;
+        }
+        for(int i = (zero + one); i< n; i++){
+            nums[i] = 2;
+        }
+    }
+}
